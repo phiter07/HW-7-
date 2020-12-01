6df250f88093253d50f900c748c050a46924ee38
Peter Nguyen <ppn4@txstate.edu>
2020-12-01 01:38:51 +0000

    meh

--- hw7.cpp
+++ hw7.cpp
@@ -14,20 +14,20 @@ class HashTest {
     list_t subtract(list_t, list_t, list_t);
 };
 
-Hash fib_sum_hash, even_or_odd_hash, old_collisions_hash, subtract_list;
+HashTest fib_sum_hash, even_or_odd_hash, old_collisions_hash, subtract_list;
 
 int old_hash1_ints, old_hash2_ints;
 
 int main() {
   list_t old_list = list_make();
-  list_t intput = list_make();
+  list_t input = list_make();
   list_t fibhash = list_make();
   list_t fibsum = list_make();
   list_t already_hashed = list_make();
   list_t final_even_odd = list_make();
   list_t diff = list_make();
 
-  for (int i = 1l i <=6; i++) {
+  for (int i = 1; i <=6; i++) {
     std:: cin >> old_hash1_ints;
     old_list = list_make(old_hash1_ints, old_list);
   }
