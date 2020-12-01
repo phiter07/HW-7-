95d91bcd788b6f49078063c1d280360c64560004
Peter Nguyen <ppn4@txstate.edu>
2020-12-01 06:29:27 +0000

    j

--- hw7.cpp
+++ hw7.cpp
@@ -1,59 +1,10 @@
+
 #include<iostream>
 #include "recursive.h"
 #include "hw4.h"
 #include <unordered_map> 
 
 using namespace std;
-
-static list_t hash_test1() {
-  list_t hash_list1 = list_make();
-  list_t empty_list = list_make();
-
-  std::string input;
-  getline(std::cin, input, ' ');
-  std::istringstream ss(input);
-  int temp;
-
-  while (ss >> temp) {
-    list_t temp_list = list_make(temp, empty_list);
-    hash_list1 = insert_list(temp_list, hash_list1, 0);
-  }
-
-  list_print(hash_list1);
-  return empty_list;
-}
-
-int main() {
-  hash_test();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
 list_t subtract(list_t, list_t, list_t);
@@ -70,38 +21,37 @@ HashTest fib_sum_hash, even_or_odd_hash, old_collisions_hash, subtract_list;
 int old_hash1_ints, old_hash2_ints;
 
 int main() {
-  list_t old_list = list_make();
-  list_t input = list_make();
-  list_t fibhash = list_make();
-  list_t fibsum = list_make();
-  list_t already_hashed = list_make();
-  list_t final_even_odd = list_make();
-  list_t diff = list_make();
+  list_t oldList = list_make();
+  list_t readInput = list_make();
+  list_t fibHash = list_make();
+  list_t fibSum = list_make();
+  list_t alreadyHashed = list_make();
+  list_t finalEvenOdd = list_make();
+  list_t diffTwo = list_make();
 
   for (int i = 1; i <=6; i++) {
     std:: cin >> old_hash1_ints;
-    old_list = list_make(old_hash1_ints, old_list);
+    oldList = list_make(old_hash1_ints, oldList);
   }
 
-  old_list = reverse(old_list);
+  oldList = reverse(oldList);
   cout << "old_hash_output";
-  list_print(old_list);
+  list_print(oldList);
 
   cout << endl;
 
   for (int i =1; i <= 6; i++) {
-    input = list_make(i, input);
+    readInput = list_make(i, readInput);
   }
 
-  input = reverse(input);
+  readInput = reverse(readInput);
 
-  fibsum = fib_sum_hash.hash_test1(old_list, input);
+  fibSum = fib_sum_hash.hash_test1(oldList, readInput);
 
-  diff = subtract(fibsum, old_list, diff);
+  diffTwo = subtract(fibSum, oldList, diffTwo);
   cout << "final_diff_output: ";
-  diff = reverse(diff);
-  list_print(diff);
+  diffTwo = reverse(diffTwo);
+  list_print(diffTwo);
   cout << endl;
 }
-
 */
